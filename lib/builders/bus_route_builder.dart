f7aabc587169ea3758f886c88906067c7fd7ed2b
Benjamin Ting <chbting@gmail.com>
2025-11-17 18:11:20 +0800

    Added bound matching

--- lib/builders/bus_route_builder.dart
+++ lib/builders/bus_route_builder.dart
@@ -5,7 +5,9 @@ import 'package:up_bus_hk_core/isar/builder_models/gov_bus_route.dart';
 import 'package:up_bus_hk_core/isar/builder_models/gov_stop.dart';
 import 'package:up_bus_hk_core/isar/models/bus_route.dart';
 import 'package:up_bus_hk_core/isar/models/bus_stop.dart';
+import 'package:up_bus_hk_data_builder/extension/lat_lng_x.dart';
 import 'package:up_bus_hk_data_builder/isar/isar_manager.dart';
+import 'package:up_bus_hk_data_builder/utils/builder_utils.dart';
 
 class BusRouteBuilder {
   static const double _routeInfoErrorDistanceMeters = 220.0; // 38X cap
@@ -63,26 +65,64 @@ class BusRouteBuilder {
     return [];
   }
 
-  static Future<GovBusRoute?> _matchGovRoute(
-    CompanyBusRoute companyRoute,
-  ) async {
+  static Future<GovBusRoute?> _matchGovRoute(CompanyBusRoute route) async {
     // 1. Filter by company & number
-    final isKmb = companyRoute.company == Company.KMB;
+    final isKmb = route.company == Company.KMB;
     final potentials = await builderIsar.govBusRoutes
         .where()
-        .numberEqualTo(companyRoute.number)
+        .numberEqualTo(route.number)
         .filter()
         .group(
           (q) => isKmb
               ? q.companyCodeContains('KMB').companyCodeContains('LWB')
-              : q.companyCodeContains(companyRoute.company.name),
+              : q.companyCodeContains(route.company.name),
         )
         .findAll();
 
     // 2. Match bound
+    final boundMatched = potentials.where((e) => _isBoundMatch(route, e));
+    //todo try matching reversed
+    //todo circular route
 
     // 3. Match stops
+    final matched = boundMatched.where((e) => _areStopsMatch(route, e));
 
-    if (potentials.isEmpty) return null;
+    return matched.isNotEmpty ? matched.first : null;
+  }
+
+  static bool _isBoundMatch(CompanyBusRoute route, GovBusRoute govRoute) {
+    final origin = busStopMap[route.stops.first]!;
+    final govOrigin = govStopMap[govRoute.stops.first]!;
+    final dest = busStopMap[route.stops.last]!;
+    final govDest = govStopMap[govRoute.stops.last]!;
+
+    // Check if the origins are the same
+    final originDistance = BuilderUtils.distance(
+      origin.latLng.toLatLong(),
+      govOrigin.latLng.toLatLong(),
+    );
+    if (originDistance > _jointRouteErrorDistanceMeters) return false;
+
+    // Check if the destinations are the same
+    final destDistance = BuilderUtils.distance(
+      dest.latLng.toLatLong(),
+      govDest.latLng.toLatLong(),
+    );
+    if (destDistance > _jointRouteErrorDistanceMeters) return false;
+
+    // For circular route, gov route omits the last stop. Check if the company
+    // route destination is the same as the gov route origin.
+    if (govRoute.originE == govRoute.destE) {
+      final terminalDistance = BuilderUtils.distance(
+        govOrigin.latLng.toLatLong(),
+        dest.latLng.toLatLong(),
+      );
+      if (terminalDistance <= _circularRouteErrorDistanceMeters) return true;
+    }
+    return true;
+  }
+
+  static bool _areStopsMatch(CompanyBusRoute route, GovBusRoute govRoute) {
+    return true;
   }
 }
--- lib/utils/builder_utils.dart
+++ lib/utils/builder_utils.dart
@@ -1,4 +1,8 @@
+import 'package:latlong2/latlong.dart';
+
 class BuilderUtils {
+  static final distance = Distance();
+
   static String formatDuration(Duration d, {bool showMs = true}) {
     return d.inSeconds < 60
         ? '${showMs ? (d.inMilliseconds / 1000) : d.inSeconds}s'
