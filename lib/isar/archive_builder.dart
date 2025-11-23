8780a95955a78e4e85c2915322370a38058523f1
Benjamin Ting <chbting@gmail.com>
2025-11-24 02:05:17 +0800

    Implemented zstd compression via process.run

--- lib/isar/archive_builder.dart
+++ lib/isar/archive_builder.dart
@@ -1,64 +1,79 @@
 import 'dart:io';
 
-import 'package:archive/archive.dart';
-import 'package:es_compression/zstd.dart';
 import 'package:intl/intl.dart';
 import 'package:isar_community/isar.dart';
 import 'package:path/path.dart';
 import 'package:up_bus_hk_core/isar/models/db_version.dart';
-import 'package:up_bus_hk_core/isar/models/transit_route.dart';
 import 'package:up_bus_hk_core/isar/up_bus_hk_schema.dart';
 import 'package:up_bus_hk_data_builder/files/project_paths.dart';
+import 'package:up_bus_hk_data_builder/utils/benchmark.dart';
 
 import 'isar_manager.dart';
 
 class ArchiveBuilder {
   static const _dateFormat = "yyyyMMdd'T'HHmmss'Z'";
+  static const _encoder = 'zstd';
 
   static Future<void> build() async {
     final isarFile = File(ProjectPath.appIsarPath);
     if (!await isarFile.exists()) throw Exception('App isar file not found');
 
     final now = DateTime.now().toUtc();
-    final created = DateFormat(_dateFormat).format(now);
-    final outName = 'UpBusHK-DB_$created.gz';
+    final createdAt = DateFormat(_dateFormat).format(now);
+    final outName = 'UpBusHK-DB_$createdAt.zst';
     final outPath = join(ProjectPath.outputDir.path, outName);
 
-    await isar.writeTxn(() => isar.dBVersions.put(DBVersion(created)));
-    final busRouteCount = await isar.busRoutes.where().count();
-    isar.close();
+    await isar.writeTxn(() => isar.dBVersions.put(DBVersion(createdAt)));
+    await isar.close();
 
-    // final input = InputFileStream(ProjectPath.appIsarPath);
-    final bytes = await isarFile.readAsBytes();
-    final encodedBytes = await zstd.encode(bytes);
-    final output = OutputFileStream(outPath);XZEncoder
-    output.writeBytes(encodedBytes);
-    await output.close();
+    final hasEncoder = await _hasEncoder();
+    if (!hasEncoder) throw ('No ${_encoder} encoder installed');
 
-    final valid = await _validate(outPath, busRouteCount);
-    valid ? print('Archive created: $outPath') : print('Archive corrupted');
+    final result = await Benchmark.executeAsync(
+      'Compressing',
+      () =>
+          Process.run('zstd', ['-22', ProjectPath.appIsarPath, '-o', outPath]),
+    );
+
+    if (result.exitCode != 0) throw Exception('Failed:\n${result.stderr}');
+
+    final valid = await _validate(outPath, createdAt);
+    valid ? print('Validated: $outPath') : print('Compress file corrupted');
   }
 
-  static Future<bool> _validate(String path, int busRouteCount) async {
-    final file = File(path);
-    if (!await file.exists()) return false;
-    final bytes = await file.readAsBytes();
-    final decodedBytes = await zstd.decode(bytes);
-    final output = OutputFileStream(
-      join(ProjectPath.outputDir.path, 'temp.isar'),
-    );
-    output.writeBytes(decodedBytes);
-    await output.close();
+  static Future<bool> _hasEncoder() async {
+    try {
+      final result = await Process.run(_encoder, ['--version']);
+      return result.exitCode == 0;
+    } catch (_) {
+      return false;
+    }
+  }
 
+  static Future<bool> _validate(String path, String createdAt) async {
+    const temp = 'temp';
+    final outPath = join(ProjectPath.outputDir.path, '$temp.isar');
+    final result = await Benchmark.executeAsync(
+      'Decompressing',
+      () => Process.run('zstd', ['-d', path, '-o', outPath]),
+    );
+    if (result.exitCode != 0) throw Exception('Failed:\n${result.stderr}');
+
+    // Validate the data is correct
     final tempIsar = await Isar.open(
       UpBusHkSchema.appSchemas,
       directory: ProjectPath.outputDir.path,
-      name: 'temp',
+      name: temp,
     );
+    final dbVersion = await tempIsar.dBVersions.where().findFirst();
+    final dbCreatedAt = dbVersion?.createdAt;
+    final valid = dbCreatedAt == createdAt;
 
-    final routeCountInTemp = await tempIsar.busRoutes.where().count();
-    tempIsar.close(deleteFromDisk: true);
+    // Clean up
+    await tempIsar.close(deleteFromDisk: true);
+    final lock = await File(join(ProjectPath.outputDir.path, '$temp.isar'));
+    if (await lock.exists()) await lock.delete();
 
-    return routeCountInTemp == busRouteCount;
+    return valid;
   }
 }
