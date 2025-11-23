801cff7f936f5e461c6b629dbd46411871c12209
Benjamin Ting <chbting@gmail.com>
2025-11-24 01:07:30 +0800

    Testing zstd compression

--- lib/isar/archive_builder.dart
+++ lib/isar/archive_builder.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:archive/archive.dart';
+import 'package:es_compression/zstd.dart';
 import 'package:intl/intl.dart';
 import 'package:isar_community/isar.dart';
 import 'package:path/path.dart';
@@ -27,25 +28,32 @@ class ArchiveBuilder {
     final busRouteCount = await isar.busRoutes.where().count();
     isar.close();
 
-    final input = InputFileStream(ProjectPath.appIsarPath);
-    final output = OutputFileStream(outPath);
-    GZipEncoder().encodeStream(input, output, level: 9);
+    // final input = InputFileStream(ProjectPath.appIsarPath);
+    final bytes = await isarFile.readAsBytes();
+    final encodedBytes = await zstd.encode(bytes);
+    final output = OutputFileStream(outPath);XZEncoder
+    output.writeBytes(encodedBytes);
+    await output.close();
 
     final valid = await _validate(outPath, busRouteCount);
     valid ? print('Archive created: $outPath') : print('Archive corrupted');
   }
 
-  static Future<bool> _validate(String gzipPath, int busRouteCount) async {
-    final archive = InputFileStream(gzipPath);
+  static Future<bool> _validate(String path, int busRouteCount) async {
+    final file = File(path);
+    if (!await file.exists()) return false;
+    final bytes = await file.readAsBytes();
+    final decodedBytes = await zstd.decode(bytes);
     final output = OutputFileStream(
       join(ProjectPath.outputDir.path, 'temp.isar'),
     );
-    GZipDecoder().decodeStream(archive, output);
+    output.writeBytes(decodedBytes);
+    await output.close();
 
     final tempIsar = await Isar.open(
       UpBusHkSchema.appSchemas,
       directory: ProjectPath.outputDir.path,
-      name: 'temp'
+      name: 'temp',
     );
 
     final routeCountInTemp = await tempIsar.busRoutes.where().count();
