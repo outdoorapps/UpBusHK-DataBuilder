4071bee7c08623b5c5a928afaf037403374871e2
Benjamin Ting <chbting@gmail.com>
2025-11-26 17:07:09 +0800

    Organized uploader

--- lib/uploader.dart
+++ lib/uploader.dart
@@ -6,6 +6,7 @@ import 'package:path/path.dart';
 import 'package:up_bus_hk_core/firebase/database_info.dart';
 import 'package:up_bus_hk_core/firebase/firebase_path.dart';
 import 'package:up_bus_hk_data_builder/files/project_path.dart';
+import 'package:up_bus_hk_data_builder/utils/benchmark.dart';
 import 'package:version/version.dart';
 
 class Uploader {
@@ -28,55 +29,59 @@ class Uploader {
     final lines = await checksumFile.readAsLines(encoding: utf8);
     final checksum = lines.first;
     final databaseFilename = lines.last;
+    final databaseFile = File(
+      join(ProjectPath.outputDir.path, databaseFilename),
+    );
 
     final parts = databaseFilename.split('_');
     final minAppVersion = Version.parse(parts[1].replaceAll('v', ''));
     final timestamp = DateTime.parse(parts[2].replaceAll('.tar.xz', ''));
-
-    // 1. Upload database file
-    final databaseFile = File(
-      join(ProjectPath.outputDir.path, databaseFilename),
-    );
-    // final success = await _upload(databaseFile);
-    // if (!success) throw Exception('Failed to upload database file');
-
-    // 2. Register database version
-    final ref = firebase.database().ref(FirebasePath.databaseInfo);
     final databaseInfo = DatabaseInfo(
       timestamp: timestamp,
       minAppVersion: minAppVersion,
       checksum: checksum,
     );
-    ref.set(databaseInfo.toMap());
-    // 3. Remove old database file
+
+    // 1. Upload database file
+    final uploaded = await Benchmark.executeAsync(
+      'Uploading database file',
+      () => _upload(firebase, databaseFile),
+    );
+    if (!uploaded) throw Exception('Failed to upload database file');
+
+    // 2. Register database version
+    final registered = await Benchmark.executeAsync(
+      'Registering database change',
+      () => _register(firebase, databaseInfo),
+    );
+    if (!registered) throw Exception('Failed to register database change');
+
+    // 3. Remove old database file todo
   }
 
-  // static Future<bool> _upload(App firebase, File databaseFile) async {
-  //   final uploadRef = firebase.storage().ref(basename(databaseFile.path));
-  //   final bytes = await databaseFile.readAsBytes();
-  //   bool success = false;
-  //   try {
-  //     await uploadRef.putData(bytes).snapshotEvents.listen((taskSnapshot) {
-  //       switch (taskSnapshot.state) {
-  //         case TaskState.running:
-  //           print('Uploading ${taskSnapshot.bytesTransferred} bytes');
-  //           break;
-  //         case TaskState.paused:
-  //           break;
-  //         case TaskState.success:
-  //           success = true;
-  //           break;
-  //         case TaskState.canceled:
-  //           break;
-  //         case TaskState.error:
-  //           break;
-  //       }
-  //     });
-  //   } on FirebaseException catch (e) {
-  //     print(e);
-  //   }
-  //   return success;
-  // }
+  static Future<bool> _upload(App firebase, File databaseFile) async {
+    try {
+      final bytes = await databaseFile.readAsBytes();
+      await firebase.storage().bucket().writeBytes(
+        basename(databaseFile.path),
+        bytes,
+      );
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+
+  static Future<bool> _register(App firebase, DatabaseInfo databaseInfo) async {
+    try {
+      final ref = firebase.database().ref(FirebasePath.databaseInfo);
+      ref.set(databaseInfo.toMap());
+    } catch (e) {
+      print(e);
+      return false;
+    }
+    return true;
+  }
 }
 
 void main() {}
