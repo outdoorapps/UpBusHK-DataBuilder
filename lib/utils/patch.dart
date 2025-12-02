776e54dd0d9b1e9d851e7c940694cdf98ac79a34
Benjamin Ting <chbting@gmail.com>
2025-12-03 01:48:29 +0800

    Correctly apply company route patch

--- lib/utils/patch.dart
+++ lib/utils/patch.dart
@@ -104,72 +104,70 @@ class Patch {
     ),
   };
 
-  static Future<void> patchCompanyRoutes() async {
+  static Future<void> patchCompanyRoutes(
+    List<CompanyBusRoute> companyRoutes,
+  ) async {
     // "60C1F7910C07C52B" for 115,I,1 KOWLOON CITY FERRY BUS TERMINUS (KC949)
     // need no matching with a CTB stop, KMB included two consecutive
     // terminating stops. This, the last one, is redundant.
-    final route115s = await builderIsar.companyBusRoutes
-        .filter()
-        .numberEqualTo('115')
-        .boundEqualTo(Bound.I)
-        .findAll();
-    route115s.forEach((route115) {
-      if (route115.stops.last == '60C1F7910C07C52B') {
-        route115.stops.removeLast();
-      }
-    });
-    await builderIsar.writeTxn(
-      () => builderIsar.companyBusRoutes.putAll(route115s),
-    );
+    companyRoutes
+        .where(
+          (e) =>
+              e.number == '115' &&
+              e.bound == Bound.I &&
+              e.company == Company.KMB,
+        )
+        .forEach((route115) {
+          if (route115.stops.last == '60C1F7910C07C52B') {
+            route115.stops.removeLast();
+          }
+        });
 
     // "F0A8A596641FFC5A" for 170,I SHA TIN STATION BUS TERMINUS (ST941)
     // needs no matching with a CTB stop, KMB included two consecutive
     // terminating stops. This, the last one, is redundant.
-    final route170s = await builderIsar.companyBusRoutes
-        .filter()
-        .numberEqualTo('170')
-        .boundEqualTo(Bound.I)
-        .findAll();
-    route170s.forEach((route170) {
-      if (route170.stops.last == 'F0A8A596641FFC5A') {
-        route170.stops.removeLast();
-      }
-    });
-    await builderIsar.writeTxn(
-      () => builderIsar.companyBusRoutes.putAll(route170s),
-    );
+    companyRoutes
+        .where(
+          (e) =>
+              e.number == '170' &&
+              e.bound == Bound.I &&
+              e.company == Company.KMB,
+        )
+        .forEach((route115) {
+          if (route115.stops.last == 'F0A8A596641FFC5A') {
+            route115.stops.removeLast();
+          }
+        });
 
     // Merge CTB route 110 outbound with inbound to make a single circular route
-    final route110Inbound = await builderIsar.companyBusRoutes
-        .filter()
-        .numberEqualTo('110')
-        .boundEqualTo(Bound.I)
-        .companyEqualTo(Company.CTB)
-        .findFirst();
+    final route110Inbound = companyRoutes
+        .where(
+          (e) =>
+              e.number == '110' &&
+              e.bound == Bound.I &&
+              e.company == Company.CTB,
+        )
+        .firstOrNull;
 
-    final route110Outbound = await builderIsar.companyBusRoutes
-        .filter()
-        .numberEqualTo('110')
-        .boundEqualTo(Bound.O)
-        .companyEqualTo(Company.CTB)
-        .findFirst();
+    final route110Outbound = companyRoutes
+        .where(
+          (e) =>
+              e.number == '110' &&
+              e.bound == Bound.O &&
+              e.company == Company.CTB,
+        )
+        .firstOrNull;
 
     if (route110Inbound != null && route110Outbound != null) {
-      final circularStops = route110Outbound.stops.toList();
+      final outboundStops = route110Outbound.stops;
       final inboundStops = route110Inbound.stops;
       final startIndex = inboundStops.indexOf(route110Outbound.stops.last);
       if (startIndex != -1 && startIndex < inboundStops.length) {
-        circularStops.addAll(
+        outboundStops.addAll(
           inboundStops.sublist(startIndex + 1, inboundStops.length),
         );
       }
-      final route110Circular = route110Outbound.copyWith(stops: circularStops);
-      route110Circular.id = route110Outbound.id;
-
-      await builderIsar.writeTxn(() async {
-        builderIsar.companyBusRoutes.delete(route110Inbound.id);
-        builderIsar.companyBusRoutes.put(route110Outbound);
-      });
+      companyRoutes.remove(route110Inbound);
     }
   }
 
